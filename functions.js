d9b5cb994f7a3ff5615c4a82341357138004eb12
chab0075 <chab0075@algonquinlive.com>
2020-06-11 09:42:20 -0400

    first commit

--- functions.js
+++ functions.js
@@ -1,3 +1,8 @@
+/*
+Noel chabot
+June 11, 2020
+*/
+
 // This function returns a random number between (and including both) minimum and maximum
 function getRandomInteger (minimum, maximum) {
   minimum = Math.ceil(minimum) // minimum is inclusive
